d620c95e8fe8d7d7d63902cb5dfb90c1c57b87b5
Pixelmaster993 <dinoresin93@gmail.com>
2025-04-08 17:34:59 +0200

    add Entities for user

---
+++ Poketcher.database/Entities/User/WantedPokemon.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Poketcher.database.Entities.User
+{
+    public class WantedPokemon
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int Generation { get; set; }
+        public bool IsShiny { get; set; }
+        public bool IsMale { get; set; }
+    }
+}
