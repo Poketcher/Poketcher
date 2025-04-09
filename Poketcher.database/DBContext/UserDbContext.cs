4d181b8e59d3ef44b741408012ac06509b427d4f
Pixelmaster993 <dinoresin93@gmail.com>
2025-04-09 09:30:32 +0200

    add UserDbContext

---
+++ Poketcher.database/DBContext/UserDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poketcher.database.Entities.User;
+
+namespace Poketcher.database.DBContext
+{
+    public class UserDbContext : DbContext
+    {
+        private readonly string _dbPath;
+
+        public UserDbContext(string dbPath, DbContextOptions<UserDbContext> options) : base(options)
+        {
+            if (string.IsNullOrWhiteSpace(dbPath))
+                throw new ArgumentException("Percorso del database non valido.", nameof(dbPath));
+
+            _dbPath = dbPath;
+        }
+
+        public DbSet<OwnedPokemon> OwnedPokemon { get; set; }
+        public DbSet<WantedPokemon> WantedPokemon { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+            }
+        }
+    }
+}
